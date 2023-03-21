d45b93806253c23ca2c56eb87b46c4ee2e8fdc35
Alexa Hernandez <111934131+AlexaHernandez06@users.noreply.github.com>
2023-03-21 21:56:47 +0000

    Tue, Mar 21, 2023, 2:56 PM -07:00

--- js/counter.js
+++ js/counter.js
@@ -6,7 +6,7 @@ counters.forEach(counter => {
     const updateCounter = () => {
         const target = +counter.getAttribute('data.target')
         const c = +counter.innerText
-        const increment = target / 2000
+        const increment = target / 200
 
         if (c < target) {
             counter.innerText = `${Math.ceil(c + increment)}`
