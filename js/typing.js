c912efe2c3fa65e6014bd9f21d9ca465249a88eb
Alexa Hernandez <111934131+AlexaHernandez06@users.noreply.github.com>
2023-03-24 20:18:38 +0000

    Fri, Mar 24, 2023, 1:18 PM -07:00

--- js/typing.js
+++ js/typing.js
@@ -5,3 +5,16 @@ let idx = 1
 let speed = 300 / speedEl.value
 
 writeText()
+
+function writetext() {
+    textEl.innerText = text.slice(0,idx)
+    idx++
+    if (idx > text.length) {
+        idx = 1
+    }
+    setTimeout(writeText, speed)
+}
+
+speedEl.addEventListener('input', (e) => {
+    speed = 300 / e.target.value
+}
