15790ff533ec7af3ad5d4441927776ad768a2f03
Alexa Hernandez <111934131+AlexaHernandez06@users.noreply.github.com>
2023-03-24 00:19:50 +0000

    Thu, Mar 23, 2023, 5:19 PM -07:00

--- js/sounds.js
+++ js/sounds.js
@@ -4,10 +4,9 @@ sounds.forEach(sound => {
     const btn = document.createElement('button')
     btn.classList.add('btn')
     btn.innerText = sound
-
     btn.addEventListener('click', () => {
         stopSongs()
-        document.getElementById(sound).play
+        document.getElementById(sound).play()
     })
 
     document.getElementById('buttons').appendChild(btn)
