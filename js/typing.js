52f4cd304b99d67c3cd393a9e5204f30b900a4d1
Alexa Hernandez <111934131+AlexaHernandez06@users.noreply.github.com>
2023-03-24 20:49:09 +0000

    Fri, Mar 24, 2023, 1:49 PM -07:00

--- js/typing.js
+++ js/typing.js
@@ -2,7 +2,7 @@ const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
 const text = 'We love programming!'
 let idx = 1
-let speed = 300
+let speed = 300 / speedEl.value
 
 writeText()
 
